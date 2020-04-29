27cb87313f0b2ba64e7e418a7ae692926b9f7545
ngawang2yeshi@gmail.com <ngawang2yeshi@gmail.com>
2020-04-29 14:11:30 -0500

    first commit

---
+++ snake_bnb/src/data/mongo_setup.py
@@ -0,0 +1,19 @@
+import mongoengine
+
+# alias_core = 'core'
+# db = 'snake_db'
+#
+# data = dict(
+#     username='user_from_config_or_env',
+#     password='password_from_config_or_env',
+#     host='server_from_config_or_env',
+#     port='port_from_config_or_env',
+#     authentication_source='admin',
+#     authentication_mechanism='SCRAM-SHA-1',
+#     ssl=True,
+#     ssl_cert_reqs='ssl.CERT_NONE'
+# )
+
+
+def global_init():
+    mongoengine.register_connection(alias='core', name='snake_bnb')
